7a90e927b8b5ae88945dae104264deb643e4b8ed
Ashmit <ashmitsharma630@gmail.com>
2025-07-08 00:58:52 +0530

    prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,29 +4,41 @@ import { connectDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js"
 import dotenv from "dotenv"
 import cors from "cors"
+import path from "path"
 
 const app = express();
 
 dotenv.config()
 
 const PORT = process.env.PORT || 5001
-
+const __dirname = path.resolve()
 
 //middleware
-app.use(cors(
-    {
-        origin: "http://localhost:5173"
-    }
-));
+if (process.env.NODE_ENV !== 'production') {
+    app.use(cors(
+        {
+            origin: "http://localhost:5173"
+        }
+    ));
+}
+
 app.use(express.json());
 app.use(rateLimiter);
-
 
 //console.log(process.env.MONGO_URI)
 //Similarly, you can create - 
 //app.use('/api/product', productRoutes)
 //app.use('/api/payments', paymentRoutes)
+
 app.use('/api/notes', notesRoutes)
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+    })
+}
 
 //This is out first API where we are listening for get request and handling it properly
 //app.get('/api/notes', (req, res) => {
